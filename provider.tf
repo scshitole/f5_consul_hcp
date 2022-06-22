6c74ebc5b8683c685da9e291d4310356457cded0
Sanjay <scshitole@gmail.com>
2022-06-22 12:59:19 -0700

    update

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "hcp" {
+  client_id     = var.service-principal-key-client-id
+  client_secret = var.service-principal-key-client-secret
+}
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "vpc_id" {
+  type        = string
+  description = "AWS VPC ID"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "AWS CIDR block"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "AWS subnet (public)"
+}
+
+variable "cluster_id" {
+  type        = string
+  description = "HCP Consul ID"
+}
+
+variable "hcp_consul_security_group_id" {
+  type        = string
+  description = "AWS Security group for HCP Consul"
+}
+
+variable "service-principal-key-client-id" {
+type        = string
+  description = "Service principle Key required for  Consul HCP"
+}
+
+variable "service-principal-key-client-secret" {
+type        = string
+  description = "Service principle key for HCP Consul HCP"
+}
+
